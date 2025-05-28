c193a4e1e2b811c559d6f55efee91e224d075b71
DASH <haseeb099m@gmail.com>
2025-05-28 15:03:44 -0700

    cleaned up

--- KLAWQ/kl-aware-quant/utils/importer.py
+++ KLAWQ/kl-aware-quant/utils/importer.py
@@ -1,37 +1,12 @@
-# Copyright 2024-2025 ModelCloud.ai
-# Copyright 2024-2025 qubitium@modelcloud.ai
-# Contact: qubitium@modelcloud.ai, x.com/qubitium
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 from collections import OrderedDict
 from typing import Dict, List, Optional, Type, Union
 
 import torch
-from gptqmodel.adapter.adapter import Adapter
 
 from ..models._const import DEVICE, normalize_device
 from ..nn_modules.qlinear import BaseQuantLinear, PackableQuantLinear
-from ..nn_modules.qlinear.bitblas import BitBLASQuantLinear
-from ..nn_modules.qlinear.exllama import ExllamaQuantLinear
-from ..nn_modules.qlinear.exllama_eora import ExllamaEoraQuantLinear
-from ..nn_modules.qlinear.exllamav2 import ExllamaV2QuantLinear
-from ..nn_modules.qlinear.ipex import IPEXQuantLinear
-from ..nn_modules.qlinear.marlin import MarlinQuantLinear
-from ..nn_modules.qlinear.qqq import QQQQuantLinear
 from ..nn_modules.qlinear.torch import TorchQuantLinear
-from ..nn_modules.qlinear.tritonv2 import TRITON_AVAILABLE, TRITON_INSTALL_HINT, TritonV2QuantLinear
 from ..quantization import FORMAT
 from ..utils.logger import setup_logger
 from . import BACKEND
@@ -42,26 +17,11 @@ message_logged = False
 log = setup_logger()
 
 AUTO_SELECT_BACKEND_ORDER = OrderedDict({
-    BACKEND.MARLIN: MarlinQuantLinear, # optimized for bs > 1
-    # BACKEND.EXLLAMA_EORA: ExllamaEoraQuantLinear, #
-    BACKEND.EXLLAMA_V2: ExllamaV2QuantLinear, # optimized for bs > 1
-    BACKEND.EXLLAMA_V1: ExllamaQuantLinear, # optimized for bs == 1
-    BACKEND.TRITON: TritonV2QuantLinear, # good all around kernel that JIT compiles
-    # BACKEND.CUDA: DynamicCudaQuantLinear,
-    BACKEND.IPEX: IPEXQuantLinear, # best kernel Intel XPU and CPU with amx/avx512/xmx
-    BACKEND.BITBLAS: BitBLASQuantLinear, # super slow AOT pre-compiler but fastest for bs=1
-    BACKEND.TORCH: TorchQuantLinear, # slightly slower than Triton but getting close in Torch 2.6.0+
-
-    BACKEND.QQQ: QQQQuantLinear, # qqq kernel based on marlin
+    BACKEND.TORCH: TorchQuantLinear,
 })
 
 FORMAT_DICT = {
-    FORMAT.GPTQ: [BACKEND.MARLIN, BACKEND.EXLLAMA_V2, BACKEND.EXLLAMA_V1, BACKEND.TRITON, BACKEND.IPEX, BACKEND.TORCH, BACKEND.MARLIN_FP16, BACKEND.EXLLAMA_EORA],
-    FORMAT.GPTQ_V2: [BACKEND.EXLLAMA_V2, BACKEND.EXLLAMA_V1, BACKEND.TRITON, BACKEND.TORCH],
-    FORMAT.MARLIN: [BACKEND.MARLIN, BACKEND.MARLIN_FP16],
-    FORMAT.BITBLAS: [BACKEND.BITBLAS],
-    FORMAT.IPEX: [BACKEND.IPEX],
-    FORMAT.QQQ: [BACKEND.QQQ],
+    FORMAT.GPTQ: [BACKEND.TORCH],
 }
 
 def normalize_device_device_map(device: Optional[Union[str, torch.device]], device_map: Optional[Union[str, Dict]]) -> Optional[DEVICE]:
@@ -71,7 +31,6 @@ def normalize_device_device_map(device: Optional[Union[str, torch.device]], devi
             devices = {device_map} if isinstance(device_map, str) else set(device_map.values())
             normalized_devices = set()
             for device in devices:
-                # Returning None means quant linear will be automatically selected.
                 if isinstance(device, str) and device == "auto":
                     return None
                 normalized_devices.add(normalize_device(device))
@@ -90,7 +49,6 @@ def normalize_device_device_map(device: Optional[Union[str, torch.device]], devi
         else:
             raise ValueError(f"device must be a string or torch.device, got {type(device)}")
 
-    # map fake cuda to actual rocm
     if normalized_device == DEVICE.CUDA and IS_ROCM:
         normalized_device = DEVICE.ROCM
     return normalized_device
@@ -101,19 +59,14 @@ def auto_select_device(device: Optional[DEVICE], backend: Optional[BACKEND]) -> 
     assert backend is None or isinstance(backend, BACKEND)
 
     if device is None:
-        if backend == BACKEND.IPEX:
-            device = DEVICE.XPU if HAS_XPU else DEVICE.CPU
-        elif HAS_CUDA:
+        if HAS_CUDA:
             device = DEVICE.CUDA
-        elif HAS_XPU:
-            device = DEVICE.XPU
         elif HAS_MPS:
             device = DEVICE.MPS
         else:
             device = DEVICE.CPU
     return device
 
-# public/stable api exposed to transformer/optimum
 def hf_select_quant_linear(
         bits: int,
         group_size: int,
@@ -125,7 +78,6 @@ def hf_select_quant_linear(
         device_map: Optional[Union[str, dict]] = None,
         backend: Optional[Union[str, BACKEND]] = None,
 ) -> Type[BaseQuantLinear]:
-    # convert hf string backend to backend.enum
     if isinstance(backend, str):
         backend = BACKEND(backend.lower())
 
@@ -143,14 +95,11 @@ def hf_select_quant_linear(
         device=device,
         format=FORMAT.GPTQ,
         pack=pack,
-        allow_marlin=True, # TODO: remove this after marlin padding is fixed
         dynamic=None,
         pack_dtype=torch.int32,
-        adapter=None,
     )
 
 
-# auto select the correct/optimal QuantLinear class
 def select_quant_linear(
         bits: int,
         group_size: int,
@@ -160,26 +109,22 @@ def select_quant_linear(
         backend: BACKEND = BACKEND.AUTO,
         format: FORMAT = FORMAT.GPTQ,
         pack: bool = False,
-        allow_marlin: bool = True,  # TODO: remove this after marlin padding is fixed
         dynamic=None,
         pack_dtype: torch.dtype = None,
-        multi_select: bool = False, # return all valid kernels
-        adapter: Optional[Adapter] = None,
+        multi_select: bool = False,
 ) -> Union[Type[BaseQuantLinear], List[Type[BaseQuantLinear]]]:
     if device is None:
-        device = DEVICE.XPU if backend == BACKEND.IPEX else DEVICE.CUDA
+        device = DEVICE.CUDA
 
     backend = BACKEND.AUTO if backend is None else backend
 
     trainable = backend == BACKEND.AUTO_TRAINABLE
 
     validated_qlinears = []
-    # Handle the case where backend is AUTO.
     if backend in [BACKEND.AUTO, BACKEND.AUTO_TRAINABLE]:
         allow_quant_linears = [(k, v) for k,v in AUTO_SELECT_BACKEND_ORDER.items() if k in FORMAT_DICT[format]]
         err = None
         global message_logged
-        # Suppose all quant linears in the model should have the same backend.
         for k, cls in allow_quant_linears:
             validate, err = cls.validate(
                 bits=bits,
@@ -190,7 +135,6 @@ def select_quant_linear(
                 dynamic=dynamic,
                 device=device,
                 trainable=trainable,
-                adapter=adapter,
             )
             if os.environ.get("DEBUG") and not validate:
                 log.info(f"skip {k} for {str(err)}")
@@ -198,17 +142,11 @@ def select_quant_linear(
                 if pack:
                     check_pack_func = issubclass(cls, PackableQuantLinear)
                     if check_pack_func:
-                        #if not message_logged:
-                        #    logger.info(f"Auto pick kernel based on compatibility: {cls}")
-                        #    message_logged = True
                         log.info(f"{'Packing' if pack else ''} Kernel: Auto-selection: adding candidate `{cls.__name__}`")
                         validated_qlinears.append(cls)
                         if not multi_select:
                             return cls
                 else:
-                    #if not message_logged:
-                    #    logger.info(f"Auto pick kernel based on compatibility: {cls}")
-                    #    message_logged = True
                     log.info(f"{'Packing' if pack else ''} Kernel: Auto-selection: adding candidate `{cls.__name__}`")
                     validated_qlinears.append(cls)
                     if not multi_select:
@@ -219,36 +157,7 @@ def select_quant_linear(
 
         return validated_qlinears
 
-    # Handle the case where backend is not AUTO.
-    if backend == BACKEND.TRITON:
-        if not TRITON_AVAILABLE:
-            raise ValueError(TRITON_INSTALL_HINT)
-        qlinear = TritonV2QuantLinear
-    elif backend == BACKEND.BITBLAS:
-        qlinear = BitBLASQuantLinear
-    elif backend in [BACKEND.MARLIN, BACKEND.MARLIN_FP16]:
-        qlinear = MarlinQuantLinear
-    elif backend == BACKEND.EXLLAMA_EORA:
-        qlinear = ExllamaEoraQuantLinear
-    elif backend == BACKEND.EXLLAMA_V2:
-        qlinear = ExllamaV2QuantLinear
-    elif backend == BACKEND.EXLLAMA_V1:
-        qlinear = ExllamaQuantLinear
-    elif backend == BACKEND.IPEX:
-        from ..nn_modules.qlinear.ipex import HAS_IPEX
-        if not HAS_IPEX:
-            raise ValueError("{'Packing' if pack else ''} Kernel: IPEX is not installed. Please install it via `pip install gptqmodel['ipex']`")
-
-        from device_smi import Device
-
-        cpu_vendor = Device("cpu").vendor
-        if cpu_vendor != "intel":
-            log.warn(f"{'Packing' if pack else ''} Kernel: IPEX on cpu is only validated and optimized for Intel cpu with AVX512, AMX, or XMX. Current cpu vendor: `{cpu_vendor}`.")
-
-        qlinear = IPEXQuantLinear
-    elif backend == BACKEND.QQQ:
-        qlinear = QQQQuantLinear
-    elif backend == BACKEND.TORCH:
+    if backend == BACKEND.TORCH:
         qlinear = TorchQuantLinear
     else:
         qlinear = TorchQuantLinear
